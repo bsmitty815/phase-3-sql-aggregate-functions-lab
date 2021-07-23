bc6ad2478d121b3c8c1a4aa27644594506c00a58
Brian Smith <brian.bubbam@gmail.com>
2021-07-23 11:10:24 -0500

    commit tree c166f23a88c484729bded9d29a9e2e837d84c0c5

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,26 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
+    #finds the highest gpa
+    "SELECT MAX(gpa) as highest_gpa FROM students;"
 end
 
 def lowest_student_gpa
+    #finds the lowest gpa
+    "SELECT MIN(gpa) as lowest_gpa FROM students;"
 end
 
 def average_student_gpa
+    #finds the average gpa
+    "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+    #finds the total amount of tardies for all students
+    "SELECT SUM(tardies) AS total_tardies FROM students;"
 end
 
 def average_gpa_for_9th_grade
+    #find the average gpa for 9th grade
+    "SELECT AVG(gpa) FROM students WHERE grade==9;"
 end
